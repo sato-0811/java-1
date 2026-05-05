8cff9bbb3d70bd1ed091d8364e85a1bc5da8747e
Ezra <zillishanron20@gmail.com>
2026-05-05 20:18:51 +0900

    Student Class 작성

--- Student.java
+++ Student.java
@@ -7,27 +7,64 @@
  */
 public class Student
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
-    private int x;
+    private String name;
+    private int StudentId;
+    private int gugudan;
 
     /**
-     * Student 클래스의 객체 생성자
-     */
-    public Student()
-    {
-        // 인스턴스 변수 초기화
-        x = 0;
-    }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
      *
      * @param  y  메소드의 샘플 파라미터
-     * @return    x 더하기 y의 결과값을 반환
+     * @return    x 와 y의 합
      */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하세요.
-        return x + y;
+    public Student(){
+        this.name=name;
+        this.StudentId=StudentId;
     }
+
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
+    public Student(String name, int StudentId, int gugudan){
+        this.name=name;
+        this.StudentId=StudentId;
+        this.gugudan=gugudan;
+    }
+    
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
+    public void calculateGugu(){
+        for(int i=1 ; i<=9; i++){
+            System.out.print(gugudan+"x"+i+"="+gugudan*i);
+        }
+    }    
+    
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
+    public void calculateGugudan(int gugudan){
+        for(int i=1 ; i<=9; i++){
+            System.out.print(gugudan+"x"+i+"="+gugudan*i);
+        }
+    }
+    
+    /**
+     * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
+     *
+     * @param  y  메소드의 샘플 파라미터
+     * @return    x 와 y의 합
+     */
+    public void setGugudan(int gugudan){
+        this.gugudan=gugudan;
+        }  
 }
