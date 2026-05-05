5fbf7f220c09f0ab060e9585a0cad1fba5e5e625
yuasagon <yuasagon@icloud.com>
2026-05-05 20:35:32 +0900

    main메소드 학생객체 생성했다.

--- MyApp.java
+++ MyApp.java
@@ -7,27 +7,12 @@
  */
 public class MyApp
 {
-    // 인스턴스 변수 - 다음의 예제를 사용자에 맞게 변경하세요.
-    private int x;
-
-    /**
-     * MyApp 클래스의 객체 생성자
-     */
-    public MyApp()
-    {
-        // 인스턴스 변수 초기화
-        x = 0;
-    }
-
-    /**
-     * 예제 메소드 - 이 주석을 사용자에 맞게 바꾸십시오
-     *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 더하기 y의 결과값을 반환
-     */
-    public int sampleMethod(int y)
-    {
-        // 여기에 코드를 작성하세요.
-        return x + y;
+    public static void main(String [] args){
+        Student s1 = new Student("JENNA",R005,4);
+        Student s2 = new Student("JAMES",R009,9);
+        Student s3 = new Student("JOHN",R010,7);
+        Student s4 = new Student("MARIA",R029,2);
+        
+        
     }
 }
