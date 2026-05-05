46e59f60d6a1563631c8dc9f28be278609f4a599
Ezra <zillishanron20@gmail.com>
2026-05-05 20:50:52 +0900

    주석 작성

--- Student.java
+++ Student.java
@@ -14,8 +14,8 @@ public class Student
     /**
      * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
      *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
+     * @param  name , studentId
+     * @return 없음
      */
     public Student(String name, int StudentId){
         this.name=name;
@@ -25,8 +25,8 @@ public class Student
     /**
      * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
      *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
+     * @param  name, StudentId, gugudan
+     * @return 없음
      */
     public Student(String name, int StudentId, int gugudan){
         this.name=name;
@@ -37,8 +37,8 @@ public class Student
     /**
      * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
      *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
+     * @param 
+     * @return 없음
      */
     public void calculateGugu(){
         for(int i=1 ; i<=9; i++){
@@ -49,8 +49,8 @@ public class Student
     /**
      * 메소드 예제 - 사용자에 맞게 주석을 바꾸십시오.
      *
-     * @param  y  메소드의 샘플 파라미터
-     * @return    x 와 y의 합
+     * @param  gugudan
+     * @return  없음
      */
     public void calculateGugudan(int gugudan){
         for(int i=1 ; i<=9; i++){
@@ -66,5 +66,5 @@ public class Student
      */
     public void setGugudan(int gugudan){
         this.gugudan=gugudan;
-        }  
+        }   
 }
