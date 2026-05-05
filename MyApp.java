900376cd220fee7390fc47a5bd94279e3dc067b8
yuasagon <yuasagon@icloud.com>
2026-05-05 20:55:25 +0900

    student메소드에 getName(),getGugudan메소드를 작성

--- MyApp.java
+++ MyApp.java
@@ -13,6 +13,24 @@ public class MyApp
         Student s3 = new Student("JOHN",R010,7);
         Student s4 = new Student("MARIA",R029,2);
         
+        //s1.JENNA학생 구구단 답하기
+        System.out.println(s1.getName() + "에게" + s1.getGugudan() + "단을 답해주세요~");
+        s1.CalculateGugudan();
+        System.out.println();
         
+        //s2.JAMES학생 구구단 답하기
+        System.out.println(s2.getName() + "에게" + s2.getGugudan() + "단을 답해주세요~");
+        s2.CalculateGugudan();
+        System.out.println();
+        
+        //s3.JOHN학생 구구단 답하기
+        System.out.println(s3.getName() + "에게" + s3.getGugudan() + "단을 답해주세요~");
+        s3.CalculateGugudan();
+        System.out.println();
+        
+        //s4.MARIA학생 구구단 답하기
+        System.out.println(s4.getName() + "에게" + s4.getGugudan() + "단을 답해주세요~");
+        s4.CalculateGugudan();
+        System.out.println();
     }
 }
